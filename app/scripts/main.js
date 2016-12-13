771cb4fb1162c52521ef9365cae4f7d630b21af3
tetchi <tetsuro.takara@gmail.com>
2016-12-12 23:07:49 -0500

    Yay fitbounds

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,9 @@ var mtlMurals = (function() {
   const MURAL_URL = 'https://jsonp.afeld.me/?url=http://donnees.ville.montreal.qc.ca/dataset/53d2e586-6e7f-4eae-89a1-2cfa7fc29fa0/resource/d325352b-1c06-4c3a-bf5e-1e4c98e0636b/download/murales.json';
   const $MURAL_INFO = $('.mural-info__title');
   const $MURAL_IMAGE = $('.mural-info__image');
+
   let map = {};
+  let markers = [];
 
   function getMuralData() {
     $.ajax({
@@ -13,21 +15,16 @@ var mtlMurals = (function() {
         plotMarkers(data);
       }
     });
-  };
+  }
 
   function initMap() {
-    let montreal = {lat: 45.5017, lng: -73.5673};
-
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: montreal
-    });
-
+    map = new google.maps.Map(document.getElementById('map'));
     getMuralData();
   }
 
   function plotMarkers(data) {
     let muralSpotsArray = data.features;
+    let bounds = new google.maps.LatLngBounds();
 
     muralSpotsArray.forEach(function(muralSpot) {
       let latitude = muralSpot.properties.latitude;
@@ -38,18 +35,27 @@ var mtlMurals = (function() {
       let marker = new google.maps.Marker({
         position: {lat: muralSpot.properties.latitude, lng: muralSpot.properties.longitude},
         map: map,
-        title: address
+        title: address,
+        animation: google.maps.Animation.DROP
       });
+
+      let position = new google.maps.LatLng(latitude, longitude);
+      bounds.extend(position);
+
+      markers.push(marker);
 
       marker.addListener('click', function() {
         $MURAL_INFO.html(address);
         $MURAL_IMAGE.attr('src', image);
       });
     });
+
+    map.fitBounds(bounds);
   }
 
   return {
-    initMap: initMap
+    initMap: initMap,
+    markers: markers
   }
 })();
 
