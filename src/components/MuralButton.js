4f902896bf728aaf62856a73badfde94342dde60
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2017-12-25 11:50:32 -0500

    Start new react

---
+++ src/components/MuralButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class MuralButton extends React.Component {
+  render() {
+    let { 
+      adresse: address, 
+      annee: year, 
+      artiste: artist,
+      image,
+      latitude,
+      longitude,
+      id,
+    } = this.props.muralData.properties;
+
+    let muralThumbnailStyle = {
+      backgroundImage: `url(${image})`,
+    };
+
+      
+    return (
+      <button className="mural-button">
+        <span 
+          className="mural-button__thumbnail" 
+          style={muralThumbnailStyle}
+        >
+        </span> 
+        <span className="mural-button__info">
+          {address}
+        </span>      
+      </button>
+    );
+  }
+
+}
+
+export default MuralButton;
