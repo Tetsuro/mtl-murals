8775af64ea6714ce11d8294101697775e57d8161
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2018-07-08 19:05:10 -0400

    Clean up Topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Topbar extends React.Component {
-  render() {
-    return (
-      <nav className="topbar">
-        <h1>MTL Murals</h1>
-      </nav>
-    );
-  }
-}
+const Topbar = () => (
+  <nav className="topbar">
+    <h1>
+      MTL Murals
+    </h1>
+  </nav>
+);
 
 export default Topbar;
